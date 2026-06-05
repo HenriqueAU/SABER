ac505bbb221a88de422fa1004e82d37921833c91
Henrique_rv <111618269+HenriqueAU@users.noreply.github.com>
2026-06-05 18:19:49 -0300

    UpdatePreferenciaGeneroDto removido
    Foi removido porque o fluxo vai ser apagar e criar um novo vínculo, não atualizar

--- backend/src/modules/preferencia-genero/dto/update-preferencia-genero.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePreferenciaGeneroDto } from './create-preferencia-genero.dto';
-
-export class UpdatePreferenciaGeneroDto extends PartialType(
-  CreatePreferenciaGeneroDto,
-) {}
