51ebd4ee9cdf67136d277fd122cd57004b53da9f
unknown <hrodriguesrabellovieira@gmail.com>
2026-06-08 01:48:16 -0300

    feat: Service e controller da entidade usuário

--- backend/src/modules/usuario/usuario.module.ts
+++ backend/src/modules/usuario/usuario.module.ts
@@ -1,8 +1,13 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { Usuario } from './usuario.entity';
+import { UsuarioController } from './usuario.controller';
+import { UsuarioService } from './usuario.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
+  controllers: [UsuarioController],
+  providers: [UsuarioService],
+  exports: [UsuarioService],
 })
 export class UsuarioModule {}
---
+++ backend/src/modules/usuario/usuario.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from './usuario.entity';
+import { Repository } from 'typeorm';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private readonly usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto): Promise<Usuario> {
+    const { instituicao_id, senha, ...dadosUsuario } = createUsuarioDto;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(senha, salt);
+    const usuario = this.usuarioRepository.create({
+      ...dadosUsuario,
+      instituicao: { id: instituicao_id },
+      senha_hash: hash,
+    });
+    return await this.usuarioRepository.save(usuario);
+  }
+
+  async findOne(id: string): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: { id },
+    });
+    if (!usuario) throw new NotFoundException('Usuário não encontrado');
+    return usuario;
+  }
+
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find();
+  }
+
+  async update(
+    id: string,
+    updateUsuarioDto: UpdateUsuarioDto,
+  ): Promise<Usuario> {
+    const usuario = await this.findOne(id);
+    const { senha, ...dadosUsuario } = updateUsuarioDto;
+    if (senha) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(senha, salt);
+      this.usuarioRepository.merge(usuario, {
+        ...dadosUsuario,
+        senha_hash: hash,
+      });
+    } else {
+      this.usuarioRepository.merge(usuario, updateUsuarioDto);
+    }
+    return await this.usuarioRepository.save(usuario);
+  }
+
+  async remove(id: string) {
+    const usuario = await this.findOne(id);
+    await this.usuarioRepository.remove(usuario);
+  }
+}
