fc35edf1e0d53392c22c53cfe8b1fa3e57b76bf6
unknown <hrodriguesrabellovieira@gmail.com>
2026-06-10 20:58:43 -0300

    feat: regras de negócio de empréstimo e usuario

--- backend/src/modules/emprestimo/emprestimo.module.ts
+++ backend/src/modules/emprestimo/emprestimo.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { Emprestimo } from './emprestimo.entity';
 import { EmprestimoController } from './emprestimo.controller';
 import { EmprestimoService } from './emprestimo.service';
+import { ExemplarService } from '../exemplar/exemplar.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Emprestimo])],
+  imports: [TypeOrmModule.forFeature([Emprestimo]), ExemplarService],
   controllers: [EmprestimoController],
   providers: [EmprestimoService],
 })
--- backend/src/modules/emprestimo/emprestimo.service.ts
+++ backend/src/modules/emprestimo/emprestimo.service.ts
@@ -4,24 +4,39 @@ import { Repository } from 'typeorm';
 import { Emprestimo } from './emprestimo.entity';
 import { CreateEmprestimoDto } from './dto/create-emprestimo.dto';
 import { UpdateEmprestimoDto } from './dto/update-emprestimo.dto';
+import { ExemplarService } from '../exemplar/exemplar.service';
+import { StatusExemplar } from '../exemplar/exemplar.entity';
 
 @Injectable()
 export class EmprestimoService {
   constructor(
     @InjectRepository(Emprestimo)
     private readonly emprestimoRepository: Repository<Emprestimo>,
+    private readonly exemplarService: ExemplarService,
   ) {}
 
   async create(createEmprestimoDto: CreateEmprestimoDto): Promise<Emprestimo> {
     const { exemplar_id, usuario_id, ...dadosEmprestimo } = createEmprestimoDto;
 
-    const emprestimo = this.emprestimoRepository.create({
-      ...dadosEmprestimo,
-      exemplar: { id: exemplar_id },
-      usuario: { id: usuario_id },
-    });
+    const exemplarDisponivel = await this.exemplarService.findOne(exemplar_id);
 
-    return await this.emprestimoRepository.save(emprestimo);
+    if (exemplarDisponivel.status !== StatusExemplar.DISPONIVEL) {
+      throw new NotFoundException('Exemplar não está disponível');
+    } else {
+      const emprestimo = this.emprestimoRepository.create({
+        ...dadosEmprestimo,
+        exemplar: { id: exemplar_id },
+        usuario: { id: usuario_id },
+      });
+
+      await this.emprestimoRepository.save(emprestimo);
+
+      await this.exemplarService.update(exemplar_id, {
+        status: StatusExemplar.EMPRESTADO,
+      });
+
+      return emprestimo;
+    }
   }
 
   async findAll(): Promise<Emprestimo[]> {
@@ -45,6 +60,11 @@ export class EmprestimoService {
   ): Promise<Emprestimo> {
     const emprestimo = await this.findOne(id);
 
+    if (updateEmprestimoDto.data_devolucao_efetiva) {
+      await this.exemplarService.update(emprestimo.exemplar.id, {
+        status: StatusExemplar.DISPONIVEL,
+      });
+    }
     this.emprestimoRepository.merge(emprestimo, updateEmprestimoDto);
 
     return await this.emprestimoRepository.save(emprestimo);
