1dbfad3007da90b7784f52f7be904acbe4e4dbca
unknown <hrodriguesrabellovieira@gmail.com>
2026-06-11 15:33:22 -0300

    feat: exceptionfilter global, cors e swagger configurado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('SABER api')
+    .setDescription('DocumentaçÃo da api do app SABER')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
