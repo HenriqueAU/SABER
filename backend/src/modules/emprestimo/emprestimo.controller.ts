df298bac6697d359dd168ad40b4886a74ceb1cb5
joviprata <joviprata07@gmail.com>
2026-06-13 19:01:02 -0300

    documentação emprestimo e preferencia-genero

--- backend/src/modules/emprestimo/emprestimo.controller.ts
+++ backend/src/modules/emprestimo/emprestimo.controller.ts
@@ -12,28 +12,35 @@ import { EmprestimoService } from './emprestimo.service';
 import { CreateEmprestimoDto } from './dto/create-emprestimo.dto';
 import { UpdateEmprestimoDto } from './dto/update-emprestimo.dto';
 import type { RequestComUser } from '../../common/interfaces/request-com-usuario.interface';
+import { ApiBearerAuth, ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Empréstimos')
+@ApiBearerAuth()
 @Controller('emprestimos')
 export class EmprestimoController {
   constructor(private readonly emprestimoService: EmprestimoService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Criar um novo empréstimo' })
   create(@Body() createEmprestimoDto: CreateEmprestimoDto) {
     return this.emprestimoService.create(createEmprestimoDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obter todos os empréstimos' })
   findAll(@Req() request: RequestComUser) {
     const instituicao_id = request.user.instituicao;
     return this.emprestimoService.findAll(instituicao_id);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obter um empréstimo específico pelo ID' })
   findOne(@Param('id') id: string) {
     return this.emprestimoService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Atualizar um empréstimo existente' })
   update(
     @Param('id') id: string,
     @Body() updateEmprestimoDto: UpdateEmprestimoDto,
@@ -42,6 +49,7 @@ export class EmprestimoController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Excluir um empréstimo' })
   remove(@Param('id') id: string) {
     return this.emprestimoService.remove(id);
   }
