0a6c9b27104e4c0999f97c16a80b59111808d720
Henrique_rv <111618269+HenriqueAU@users.noreply.github.com>
2026-06-14 20:50:12 -0300

    Typos nas descrições

--- backend/src/modules/resposta_membro/dto/create-resposta-membro.dto.ts
+++ backend/src/modules/resposta_membro/dto/create-resposta-membro.dto.ts
@@ -4,14 +4,14 @@ import { IsNotEmpty, IsUUID } from 'class-validator';
 export class CreateRespostaMembroDto {
 
   @ApiProperty({
-      description: 'ID da mebro do clube relacionada',
+      description: 'ID do membro relacionado ao clube',
       example: 'de81b0ba-8ae7-4fd3-94ab-7ae3f25cf84b'})
   @IsUUID()
   @IsNotEmpty()
   membro_id!: string;
 
   @ApiProperty({
-      description: 'ID do item perugunta relacionada',
+      description: 'ID do item da pergunta relacionado ao clube',
       example: 'de81b0ba-8ae7-4fd3-94ab-7ae3f25cf84b'})
   @IsUUID()
   @IsNotEmpty()
