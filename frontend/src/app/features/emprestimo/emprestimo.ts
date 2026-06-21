54edcdb42d6570ddae61ecb290f7907f28444aa3
joviprata <joviprata07@gmail.com>
2026-06-21 19:56:03 -0300

    implementação tela Emprestimo

--- frontend/src/app/features/emprestimo/emprestimo.ts
+++ frontend/src/app/features/emprestimo/emprestimo.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Modal } from 'bootstrap';
 import { EmpréstimosService } from '../../../client/services/emprestimos.service';
@@ -7,16 +7,22 @@ import { ExemplaresService } from '../../../client/services/exemplares.service';
 import { CommonModule } from '@angular/common';
 import { LivrosService } from '../../../client/services/livros.service';
 import { RouterLink } from '@angular/router';
+import { UsuáriosService } from '../../../client/services/usuarios.service';
 
 @Component({
   selector: 'app-emprestimo',
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './emprestimo.html',
   styleUrl: './emprestimo.css',
 })
 export default class EmprestimoComponent {
   emprestimoForm = new FormGroup({
+
     nome_aluno: new FormControl('', {
+      validators: []
+    }),
+
+    aluno_id: new FormControl('', {
       validators: []
     }),
 
@@ -30,13 +36,22 @@ export default class EmprestimoComponent {
   });
 
   livros$: Observable<any>;
+  livroSelecionado: any = null;
 
   exemplares: any[] = [];
+  exemplarSelecionado: any = null;
+
+  alunos: any[] = [];
+  alunosFiltrados: any[] = [];
+  alunoSelecionado: any = null;
+
+  termoPesquisa = '';
 
   constructor(
     protected emprestimosService: EmpréstimosService,
-    protected exemplaresService: ExemplaresService,
     protected livrosService: LivrosService,
+    protected exemplaresService: ExemplaresService,
+    protected usuariosService: UsuáriosService,
   ) {
     this.livros$ = this.livrosService.livroControllerFindAll();
 
@@ -45,6 +60,51 @@ export default class EmprestimoComponent {
       .subscribe(data => {
         this.exemplares = data;
       });
+
+    this.usuariosService
+      .usuarioControllerFindAll()
+      .subscribe(data => {
+        this.alunos = data.filter(
+          (usuario: any) => usuario.perfil === 'aluno'
+        );
+      });
+  }
+
+  abrirModalExemplares(livro: any) {
+    this.livroSelecionado = livro;
+
+    const modal = new Modal(
+      document.getElementById('exemplaresModal')!
+    );
+
+    modal.show();
+  }
+
+  selecionarExemplar(exemplar: any) {
+    this.exemplarSelecionado = exemplar;
+
+    const exemplaresModal =
+      Modal.getInstance(
+        document.getElementById('exemplaresModal')!
+      );
+
+    exemplaresModal?.hide();
+
+    const emprestimoModal = new Modal(
+      document.getElementById('emprestimoModal')!
+    );
+
+    emprestimoModal.show();
+  }
+
+  selecionarAluno(aluno: any) {
+    this.alunoSelecionado = aluno;
+
+    this.emprestimoForm.patchValue({
+      aluno_id: aluno.id
+    });
+
+    this.alunosFiltrados = [];
   }
 
   private abrirModalSucesso() {
@@ -58,10 +118,42 @@ export default class EmprestimoComponent {
     modal.show();
   }
 
+  filtrarLivros(livros: any[]) {
+
+    if (!this.termoPesquisa.trim()) {
+      return livros;
+    }
+
+    const termo = this.termoPesquisa.toLowerCase();
+
+    return livros.filter((livro: any) =>
+      livro.titulo.toLowerCase().includes(termo)
+      ||
+      livro.autor.toLowerCase().includes(termo)
+    );
+
+  }
+
+filtrarAlunos() {
+  const termo =
+    this.emprestimoForm.get('nome_aluno')?.value
+      ?.toLowerCase() || '';
+
+  if (!termo) {
+    this.alunosFiltrados = [];
+    return;
+  }
+
+  this.alunosFiltrados =
+    this.alunos.filter((aluno: any) =>
+      aluno.nome.toLowerCase().includes(termo)
+    );
+
+}
+
   getExemplaresLivro(livroId: string) {
     return this.exemplares.filter(
       exemplar => exemplar.livro.id === livroId,
-      console.log(this.exemplares[0])
     );
   }
 
@@ -98,7 +190,7 @@ export default class EmprestimoComponent {
     const qtdeExemplares = exemplares.length;
 
     if (qtdeExemplares === 0) {
-      return 'bg-secondary text-white';
+      return 'bg-secondary-subtle text-secondary';
     }
 
     const qtdeDisponiveis = exemplares.filter(
@@ -106,15 +198,40 @@ export default class EmprestimoComponent {
     ).length;
 
     if (qtdeDisponiveis === 0) {
-      return 'bg-danger';
+      return 'bg-danger-subtle text-danger';
     }
 
-    return 'bg-success';
+    return 'bg-success-subtle text-success';
   }
 
   onSubmit() {
 
+    if (!this.exemplarSelecionado) {
+      return;
+    }
 
-    this.abrirModalSucesso();
+    const payload = {
+      exemplar_id: this.exemplarSelecionado.id,
+      usuario_id: this.emprestimoForm.value.aluno_id,
+      data_retirada: new Date(),
+      data_devolucao_esperada:
+        this.emprestimoForm.value.data_devolucao_esperada
+    };
+
+    this.emprestimosService
+      .emprestimoControllerCreate(payload as any)
+      .subscribe(() => {
+
+        const emprestimoModal =
+          Modal.getInstance(
+            document.getElementById('emprestimoModal')!
+          );
+
+        emprestimoModal?.hide();
+
+        this.abrirModalSucesso();
+
+      });
+
   }
 }
