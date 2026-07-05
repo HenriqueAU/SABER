2bb8368114dd4de91192fd48dc6f1c415c719919
joviprata <joviprata07@gmail.com>
2026-07-05 16:45:20 -0300

    estilização e ajustes em livro

--- frontend/src/app/features/livro/livro.ts
+++ frontend/src/app/features/livro/livro.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, ChangeDetectorRef } from "@angular/core";
+import { Component, inject, OnInit, ChangeDetectorRef, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { LivrosService } from '../../../client/services/livros.service';
@@ -10,6 +10,7 @@ import GenerosComponent from './generos/generos';
 import { LivroGeneroService } from './generos/livro-genero.service';
 import { catchError, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { Modal } from "bootstrap";
 
 interface Livro extends CreateLivroDto {
   id: string;
@@ -21,31 +22,12 @@ interface Livro extends CreateLivroDto {
   templateUrl: './livro.html',
   styleUrl: './livro.scss',
 })
-export default class LivroComponent implements OnInit {
+export default class LivroComponent implements OnInit, AfterViewInit {
   private livrosService = inject(LivrosService);
   private authService = inject(CoreAuthService);
   private fb = inject(FormBuilder);
   private cdr = inject(ChangeDetectorRef);
   private livroGeneroService = inject(LivroGeneroService);
-
-  livros: Livro[] = [];
-  mostrarForm = false;
-  livroSelecionado: Livro | null = null;
-  livroDetalhes: Livro | null = null;
-  generosPorLivro: Record<string, string[]> = {};
-  buscandoIsbn = false;
-
-  termoBusca ='';
-  generoSelecionado = '';
-  generosDisponiveis: string[] = [];
-  
-  trackById(index: number, livro: Livro): string {
-    return livro.id;
-  }
-
-  get isBibliotecario(): boolean {
-    return this.authService.getPerfil() === TipoPerfil.BIBLIOTECARIO;
-  }
 
   livroForm: FormGroup = this.fb.group({
     titulo: ['', Validators.required],
@@ -58,12 +40,44 @@ export default class LivroComponent implements OnInit {
     capa_url: ['']
   });
 
+  @ViewChild('livroDetalhesModal')
+  livroDetalhesModalRef!: ElementRef;
+
+  @ViewChild('livroFormModal')
+  livroFormModalRef!: ElementRef;
+
+  @ViewChild('successModal')
+  successModalRef!: ElementRef;
+
+  @ViewChild('errorModal')
+  errorModalRef!: ElementRef;
+
+  livros: Livro[] = [];
+  livroSelecionado: Livro | null = null;
+  livroDetalhes: Livro | null = null;
+  generosPorLivro: Record<string, string[]> = {};
+  buscandoIsbn = false;
+  mensagemSucessoModal: string = 'Operação realizada com sucesso!';
+  mensagemErroModal: string = 'Ocorreu um erro na operação.';
+
+  termoPesquisa ='';
+  generoSelecionado = '';
+  generosDisponiveis: string[] = [];
+  
+  trackById(index: number, livro: Livro): string {
+    return livro.id;
+  }
+
+  get isBibliotecario(): boolean {
+    return this.authService.getPerfil() === TipoPerfil.BIBLIOTECARIO;
+  }
+
   get livrosFiltrados(): Livro[] {
     return this.livros.filter((livro)=>{
       const textoOk = 
-        !this.termoBusca ||
-        livro.titulo.toLowerCase().includes(this.termoBusca.toLowerCase()) ||
-        livro.autor.toLowerCase().includes(this.termoBusca.toLowerCase());
+        !this.termoPesquisa ||
+        livro.titulo.toLowerCase().includes(this.termoPesquisa.toLowerCase()) ||
+        livro.autor.toLowerCase().includes(this.termoPesquisa.toLowerCase());
       
       const generoDoLivro = this.generosPorLivro[livro.id] ?? [];
       const generoOk = !this.generoSelecionado || generoDoLivro.includes(this.generoSelecionado);
@@ -72,88 +86,125 @@ export default class LivroComponent implements OnInit {
     })
   }
 
-ngOnInit() {
-  this.carregarLivros();
+  ngOnInit() {
+    this.carregarLivros();
 
-  this.livroForm.get('isbn')?.valueChanges.pipe(
-    debounceTime(600),
-    distinctUntilChanged(),
-    filter((isbn: string) => !!isbn && isbn.length >= 10),
-    switchMap((isbn: string) => {
-    this.buscandoIsbn = true;
-    return this.livrosService.livroControllerBuscarPorIsbn(isbn).pipe(
-      catchError(() => of(null))
-  );
-})
-  ).subscribe((dados: any) => {
-    this.buscandoIsbn = false;
-    if (!dados) return;
-    if (dados.titulo) this.livroForm.patchValue({ titulo: dados.titulo }, { emitEvent: false });
-    if (dados.autor) this.livroForm.patchValue({ autor: dados.autor }, { emitEvent: false });
-    if (dados.capa_url) this.livroForm.patchValue({ capa_url: dados.capa_url }, { emitEvent: false });
-    if (dados.editora) this.livroForm.patchValue({ editora: dados.editora }, { emitEvent: false });
-    if (dados.publicado_em){
-      const ano = new Date(dados.publicado_em).getFullYear();
-       if (!isNaN(ano)) this.livroForm.patchValue({ ano_publicacao: ano }, { emitEvent: false });
+    this.livroForm.get('isbn')?.valueChanges.pipe(
+      debounceTime(600),
+      distinctUntilChanged(),
+      filter((isbn: string) => !!isbn && isbn.length >= 10),
+      switchMap((isbn: string) => {
+      this.buscandoIsbn = true;
+      return this.livrosService.livroControllerBuscarPorIsbn(isbn).pipe(
+        catchError(() => of(null))
+    );
+  })
+    ).subscribe((dados: any) => {
+      this.buscandoIsbn = false;
+      if (!dados) return;
+      if (dados.titulo) this.livroForm.patchValue({ titulo: dados.titulo }, { emitEvent: false });
+      if (dados.autor) this.livroForm.patchValue({ autor: dados.autor }, { emitEvent: false });
+      if (dados.capa_url) this.livroForm.patchValue({ capa_url: dados.capa_url }, { emitEvent: false });
+      if (dados.editora) this.livroForm.patchValue({ editora: dados.editora }, { emitEvent: false });
+      if (dados.publicado_em){
+        const ano = new Date(dados.publicado_em).getFullYear();
+        if (!isNaN(ano)) this.livroForm.patchValue({ ano_publicacao: ano }, { emitEvent: false });
+      }
+      if (dados.sinopse) this.livroForm.patchValue({ sinopse: dados.sinopse }, { emitEvent: false });
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.livroFormModalRef.nativeElement.addEventListener(
+      'hidden.bs.modal',
+      () => {
+        this.livroSelecionado = null;
+        this.livroForm.reset({ faixa_etaria: 'livre'});
+      }
+    )
+  }
+
+  carregarLivros() {
+    this.livrosService.livroControllerFindAll().subscribe({
+      next: (dados) => {
+        this.livros = dados;
+        this.carregarGeneros();
+      }
+    });
+  }
+
+  carregarGeneros() {
+    this.livroGeneroService.listar().subscribe({
+      next: (relacoes) => {
+        this.generosPorLivro = {};
+        const generosSet = new Set<string>();
+
+        relacoes.forEach((relacao: any) => {
+          const livroId = relacao.livro.id;
+          const nomeGenero = relacao.genero.nome;
+
+          if (!this.generosPorLivro[livroId]) {
+            this.generosPorLivro[livroId] = [];
+          }
+          this.generosPorLivro[livroId].push(nomeGenero);
+          generosSet.add(nomeGenero);
+        });
+
+        this.generosDisponiveis = Array.from(generosSet).sort();
+        this.cdr.detectChanges();
+      },
+    });
+  }
+
+  abrirLivroDetalhesModal(livro: Livro) {
+    this.livroDetalhes = livro;
+
+    this.cdr.detectChanges();
+
+    const modal = new Modal(
+      this.livroDetalhesModalRef.nativeElement
+    );
+    
+    modal.show();
+  }
+
+  abrirLivroFormModal(livro?: Livro) {
+    if (livro) {
+      this.livroSelecionado = livro;
+      this.livroForm.patchValue(livro);
     }
-    if (dados.sinopse) this.livroForm.patchValue({ sinopse: dados.sinopse }, { emitEvent: false });
-  });
-}
-
- carregarLivros() {
-  this.livrosService.livroControllerFindAll().subscribe({
-    next: (dados) => {
-      this.livros = dados;
-      this.carregarGeneros();
+    else {
+      this.livroSelecionado = null;
+      this.livroForm.reset({ faixa_etaria: 'livre' });
     }
-  });
-}
 
-carregarGeneros() {
-  this.livroGeneroService.listar().subscribe({
-    next: (relacoes) => {
-      this.generosPorLivro = {};
-      const generosSet = new Set<string>();
-
-      relacoes.forEach((relacao: any) => {
-        const livroId = relacao.livro.id;
-        const nomeGenero = relacao.genero.nome;
-
-        if (!this.generosPorLivro[livroId]) {
-          this.generosPorLivro[livroId] = [];
-        }
-        this.generosPorLivro[livroId].push(nomeGenero);
-        generosSet.add(nomeGenero);
-      });
-
-      this.generosDisponiveis = Array.from(generosSet).sort();
-      this.cdr.detectChanges();
-    },
-  });
-}
-  abrirFormCadastro() {
-    this.livroSelecionado = null;
-    this.livroForm.reset({ faixa_etaria: 'livre' });
-    this.mostrarForm = true;
+    const modal = new Modal(
+      this.livroFormModalRef.nativeElement
+    );
+    
+    modal.show();
   }
 
-  abrirFormEdicao(livro: Livro) {
-    this.livroSelecionado = livro;
-    this.livroForm.patchValue(livro);
-    this.mostrarForm = true;
+  private abrirModalSucesso() {
+    const successModalElement = this.successModalRef.nativeElement;
+
+    if (!successModalElement) {
+      return;
+    }
+
+    const modal = new Modal(successModalElement);
+    modal.show();
   }
 
-  fecharForm() {
-    this.mostrarForm = false;
-    this.livroSelecionado = null;
-  }
+  private abrirModalErro() {
+    const errorModalElement = this.errorModalRef.nativeElement;
 
-  abrirDetalhes(livro: Livro) {
+    if (!errorModalElement) {
+      return;
+    }
 
-  this.livroDetalhes = livro;
-}
-  fecharDetalhes() {
-    this.livroDetalhes = null;
+    const modal = new Modal(errorModalElement);
+    modal.show();
   }
 
   onLivroSalvo() {
@@ -164,19 +215,69 @@ carregarGeneros() {
     if (this.livroSelecionado) {
       this.livrosService.livroControllerUpdate(this.livroSelecionado.id, formValue).subscribe({
         next: () => {
-          this.fecharForm();
+          const livroFormModal =
+            Modal.getInstance(
+              this.livroFormModalRef.nativeElement
+            );
+
+          const tituloLivro = this.livroForm.get('titulo')?.value;
+
+          livroFormModal?.hide();
+
+          this.mensagemSucessoModal = `Livro "${tituloLivro}" editado com sucesso!`;
+          this.abrirModalSucesso();
+          
           this.carregarLivros();
+          this.cdr.detectChanges();
+        },
+        error: () => {
+          const livroFormModal =
+            Modal.getInstance(
+              this.livroFormModalRef.nativeElement
+            );
+          
+          const tituloLivro = this.livroForm.get('titulo')?.value;
+
+          livroFormModal?.hide();
+
+          this.mensagemErroModal = `Erro ao editar o livro "${tituloLivro}".`;
+          this.abrirModalErro();
+          this.cdr.detectChanges();
         },
       });
     } else {
       this.livrosService.livroControllerCreate(formValue).subscribe({
         next: () => {
-          this.fecharForm();
+          const livroFormModal =
+            Modal.getInstance(
+              this.livroFormModalRef.nativeElement
+            );
+
+          const tituloLivro = this.livroForm.get('titulo')?.value;
+
+          livroFormModal?.hide();
+
+          this.mensagemSucessoModal = `Livro "${tituloLivro}" cadastrado com sucesso!`;
+          this.abrirModalSucesso();
+          
           this.carregarLivros();
+          this.cdr.detectChanges();
+        },
+        error: () => {
+          const livroFormModal =
+            Modal.getInstance(
+              this.livroFormModalRef.nativeElement
+            );
+
+          const tituloLivro = this.livroForm.get('titulo')?.value;
+
+          livroFormModal?.hide();
+
+          this.mensagemErroModal = `Erro ao cadastrar o livro "${tituloLivro}".`;
+          this.abrirModalErro();
+          this.cdr.detectChanges();
         },
       });
     }
   }
 }
-
-
