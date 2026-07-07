16a6f359f4ae43070ecb142c669392712e9e3605
joviprata <joviprata07@gmail.com>
2026-07-07 15:45:17 -0300

    pref gêneros em perfil (readicionado)

--- frontend/src/app/shared/pages/perfil/perfil.ts
+++ frontend/src/app/shared/pages/perfil/perfil.ts
@@ -1,10 +1,12 @@
 import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { CoreAuthService } from '../../../core/auth/auth-session';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterLink } from "@angular/router";
+import { RouterLink } from "@angular/router";
+import { forkJoin } from 'rxjs';
 import { UsuariosService } from '../../../../client/services/usuarios.service';
 import { CommonModule, Location } from '@angular/common';
 import { Modal } from 'bootstrap';
+import { GenerosService, PreferenciasGeneroService } from '../../../../client';
 
 @Component({
   selector: 'app-perfil',
@@ -15,11 +17,14 @@ import { Modal } from 'bootstrap';
 export default class PerfilComponent implements OnInit{
   private coreAuthService = inject(CoreAuthService);
   private usuariosService = inject(UsuariosService);
-  private router = inject(Router);
+  protected preferenciasGeneroService = inject(PreferenciasGeneroService);
+  protected generosService = inject(GenerosService);
   private location = inject(Location);
 
   usuarioId!: string;
   usuarioData: any = null;
+
+  tipoPerfil: string | null = null;
 
   userForm = new FormGroup({
     nome: new FormControl('', {
@@ -36,50 +41,132 @@ export default class PerfilComponent implements OnInit{
   @ViewChild('successModal')
   successModalRef!: ElementRef;
 
-  ngOnInit(): void {
-    const token = this.coreAuthService.getToken();
-    if (token) {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const payload = JSON.parse(atob(base64));
-      this.usuarioId = payload.id;
+  generos$? = this.generosService.generoControllerFindAll();
 
-      this.carregarDadosUsuario();
+  preferenciasOriginais: any[] = [];
+
+  generosSelecionados: string[] = [];
+
+  ngOnInit(): void {
+    this.usuarioId = this.coreAuthService.getId();
+    this.tipoPerfil = this.coreAuthService.getPerfil();
+
+    this.usuariosService.usuarioControllerFindOne(this.usuarioId).subscribe(usuario => {
+      this.userForm.patchValue(usuario);
+    })
+
+    if (this.tipoPerfil !== 'aluno') {
+      return;
     }
+
+    this.preferenciasGeneroService
+      .preferenciaGeneroControllerFindAll()
+      .subscribe(data => {
+        this.preferenciasOriginais = data;
+
+        this.generosSelecionados = this.preferenciasOriginais.map(
+          (pref: any) => pref.genero.id
+        );
+      });
+  }
+  
+  abrirModalSucesso() {
+    const successModalElement = this.successModalRef.nativeElement;
+
+    if (!successModalElement) {
+      return;
+    }
+
+    const modal = new Modal(successModalElement);
+    modal.show();
   }
 
-  carregarDadosUsuario() {
-    this.usuariosService.usuarioControllerFindOne(this.usuarioId).subscribe(data => {
-      this.usuarioData = data;
-      this.userForm.patchValue({
-        nome: data.nome,
-        email: data.email,
+  carregarPreferencias() {
+    this.preferenciasGeneroService
+      .preferenciaGeneroControllerFindAll()
+      .subscribe(data => {
+        this.preferenciasOriginais = data;
+
+        this.generosSelecionados = this.preferenciasOriginais.map(
+          (pref: any) => pref.genero.id
+        );
       });
-    });
   }
 
   cancelarEdicao() {
     this.location.back();
   }
 
-  onSubmit() {
-    if (this.userForm.invalid) return;
-
-    this.usuariosService.usuarioControllerUpdate(this.usuarioId, this.userForm.value as any).subscribe({
-      next: () => {
-        this.abrirModalSucesso();
-      }
-    });
-  }
-
-  abrirModalSucesso() {
-    const modal = new Modal(this.successModalRef.nativeElement);
-    modal.show();
-  }
-
   onReturn() {
     const modal = Modal.getInstance(this.successModalRef.nativeElement);
     modal?.hide();
     this.location.back();
+  }
+
+  onSubmit() {
+    if (this.userForm.invalid) return;
+
+    this.usuariosService.usuarioControllerUpdate(
+      this.usuarioId,
+      this.userForm.value as any
+    ).subscribe(() => {
+      if (this.tipoPerfil !== 'aluno') {
+        this.abrirModalSucesso();
+        return;
+      }
+
+      const idsOriginais = this.preferenciasOriginais.map(
+        pref => pref.genero.id
+      );
+
+      const idsAtuais = this.generosSelecionados;
+
+      const paraCriar = idsAtuais.filter(
+        id => !idsOriginais.includes(id)
+      );
+
+      const paraRemover = this.preferenciasOriginais.filter(
+        pref => !idsAtuais.includes(pref.genero.id)
+      );
+
+      const requests = [
+        ...paraCriar.map(generoId =>
+          this.preferenciasGeneroService
+            .preferenciaGeneroControllerCreate({
+              usuario_id: this.usuarioId,
+              genero_id: generoId,
+            })
+        ),
+
+        ...paraRemover.map(pref =>
+          this.preferenciasGeneroService
+            .preferenciaGeneroControllerRemove(pref.id)
+        )
+      ];
+
+      if (requests.length === 0) {
+        this.carregarPreferencias();
+        this.abrirModalSucesso();
+        return;
+      }
+
+      forkJoin(requests).subscribe(() => {
+        this.carregarPreferencias();
+        this.abrirModalSucesso();
+      });
+    })
+  }
+
+  toggleGenero(id: string) {
+    const index = this.generosSelecionados.indexOf(id);
+    if (index >= 0) {
+      this.generosSelecionados.splice(index, 1);
+      return;
+    }
+    this.generosSelecionados.push(id);
+  }
+
+  isGeneroSelecionado(id: string): boolean {
+    return this.generosSelecionados.includes(id);
   }
 }
