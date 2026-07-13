aa201030a39ae0f49e1100603cf5f54b1d8fab91
Thamires <thamiresdosanjosdasilva8.com>
2026-07-13 09:40:20 -0300

    Pesquisa aluno por email

--- frontend/src/app/features/usuario/usuario.ts
+++ frontend/src/app/features/usuario/usuario.ts
@@ -40,7 +40,8 @@ export default class UsuarioComponent implements OnInit {
     this.usuarios().filter((usuario) => {
       const nomeOk =
         !this.termoBusca() ||
-        usuario.nome.toLowerCase().includes(this.termoBusca().toLowerCase());
+        usuario.nome.toLowerCase().includes(this.termoBusca().toLowerCase()) ||
+        usuario.email.toLowerCase().includes(this.termoBusca().toLocaleLowerCase());
       const perfilOk =
         !this.PerfilSelecionado() ||
         usuario.perfil === this.PerfilSelecionado();
