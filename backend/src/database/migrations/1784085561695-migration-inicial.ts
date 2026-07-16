527d7a9b8384f26bd5fbaac0650b4e3d57e836fe
Henrique_rv <111618269+HenriqueAU@users.noreply.github.com>
2026-07-16 14:35:08 -0300

    Refactor initial migration by removing index drops
    Removed several index drop queries and modified the 'data' column in the 'notificacao' table.

--- backend/src/database/migrations/1784085561695-migration-inicial.ts
+++ backend/src/database/migrations/1784085561695-migration-inicial.ts
@@ -4,11 +4,6 @@ export class MigrationInicial1784085561695 implements MigrationInterface {
   name = 'MigrationInicial1784085561695';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."idx_email"`);
-    await queryRunner.query(`DROP INDEX "public"."idx_instituicao_id_usuario"`);
-    await queryRunner.query(`DROP INDEX "public"."idx_instituicao_id_livro"`);
-    await queryRunner.query(`DROP INDEX "public"."idx_livro_id"`);
-    await queryRunner.query(`DROP INDEX "public"."idx_usuario_id"`);
     await queryRunner.query(
       `CREATE TYPE "public"."emprestimo_status_enum" AS ENUM('ativo', 'devolvido', 'perdido', 'danificado')`,
     );
@@ -18,34 +13,13 @@ export class MigrationInicial1784085561695 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "exemplar" ALTER COLUMN "status" SET DEFAULT 'disponivel'`,
     );
-    await queryRunner.query(`ALTER TABLE "notificacao" DROP COLUMN "data"`);
-    await queryRunner.query(
-      `ALTER TABLE "notificacao" ADD "data" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "notificacao" DROP COLUMN "data"`);
-    await queryRunner.query(
-      `ALTER TABLE "notificacao" ADD "data" TIMESTAMP NOT NULL DEFAULT now()`,
-    );
     await queryRunner.query(
       `ALTER TABLE "exemplar" ALTER COLUMN "status" DROP DEFAULT`,
     );
     await queryRunner.query(`ALTER TABLE "emprestimo" DROP COLUMN "status"`);
     await queryRunner.query(`DROP TYPE "public"."emprestimo_status_enum"`);
-    await queryRunner.query(
-      `CREATE INDEX "idx_usuario_id" ON "emprestimo" ("usuario_id") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "idx_livro_id" ON "exemplar" ("livro_id") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "idx_instituicao_id_livro" ON "livro" ("instituicao_id") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "idx_instituicao_id_usuario" ON "usuario" ("instituicao_id") `,
-    );
-    await queryRunner.query(`CREATE INDEX "idx_email" ON "usuario" ("email") `);
   }
 }
